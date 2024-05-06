865231278e8593c599ef99b8d163d8420968b05e
imakiri <vanzha.alexander@gmail.com>
2024-05-06 13:23:53 +0600

    added Integer, Number, Float, Bool, Bytes, Any functions

--- record/record.go
+++ record/record.go
@@ -1,7 +1,9 @@
 package record
 
 import (
+	"encoding/base64"
 	"fmt"
+	"reflect"
 	"strconv"
 )
 
@@ -36,6 +38,27 @@ func Int(key string, value int) Record {
 	return Record{
 		key:   key,
 		value: strconv.Itoa(value),
+	}
+}
+
+func Integer(key string, value int64) Record {
+	return Record{
+		key:   key,
+		value: strconv.FormatInt(value, 10),
+	}
+}
+
+func Number(key string, value uint64) Record {
+	return Record{
+		key:   key,
+		value: strconv.FormatUint(value, 10),
+	}
+}
+
+func Float(key string, value float64) Record {
+	return Record{
+		key:   key,
+		value: strconv.FormatFloat(value, 'e', -1, 64),
 	}
 }
 
@@ -75,5 +98,61 @@ func Error(key string, err error) ErrorRecord {
 	return ErrorRecord{
 		key:   key,
 		error: err,
+	}
+}
+
+func Bool(key string, value bool) Record {
+	return Record{
+		key:   key,
+		value: strconv.FormatBool(value),
+	}
+}
+
+func Bytes(key string, value []byte) Record {
+	return Record{
+		key:   key,
+		value: base64.StdEncoding.EncodeToString(value),
+	}
+}
+
+func Any(key string, value any) []Record {
+	return parse(key, reflect.ValueOf(value), nil)
+}
+
+func parse(key string, v reflect.Value, records []Record) []Record {
+	switch v.Kind() {
+	case reflect.Pointer:
+		return append(records, parse(key, v.Elem(), records)...)
+	case reflect.String:
+		return append(records, String(key, v.String()))
+	case reflect.Int, reflect.Int64, reflect.Int8, reflect.Int16, reflect.Int32:
+		return append(records, Integer(key, v.Int()))
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		return append(records, Number(key, v.Uint()))
+	case reflect.Bool:
+		return append(records, Bool(key, v.Bool()))
+	case reflect.Float32, reflect.Float64:
+		return append(records, Float(key, v.Float()))
+	case reflect.Struct:
+		for i := 0; i < v.NumField(); i++ {
+			records = parse(fmt.Sprintf("%s.%s", key, v.Type().Field(i).Name), v.Field(i), records)
+		}
+		return records
+	case reflect.Map:
+		var iter = v.MapRange()
+		for iter.Next() {
+			records = parse(fmt.Sprintf("%s.%s", key, iter.Key()), iter.Value(), records)
+		}
+		return records
+	case reflect.Array, reflect.Slice:
+		if reflect.TypeOf([]byte(nil)) == v.Type() {
+			return append(records, Bytes(key, v.Bytes()))
+		}
+		for i := 0; i < v.Len(); i++ {
+			records = parse(fmt.Sprintf("%s[%d]", key, i), v.Index(i), records)
+		}
+		return records
+	default:
+		return records
 	}
 }
---
+++ record/record_test.go
@@ -0,0 +1,51 @@
+package record
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/stretchr/testify/require"
+	"os"
+	"testing"
+)
+
+type testStruct2 struct {
+	A string
+	T []int64
+}
+
+type testStruct struct {
+	Foo  int
+	Bar  string
+	Buzz bool
+	testStruct2
+}
+
+func TestStruct1(t *testing.T) {
+	var s = testStruct{
+		Foo:  12,
+		Bar:  "fgh",
+		Buzz: true,
+		testStruct2: testStruct2{
+			A: "123",
+			T: []int64{8, 98},
+		},
+	}
+
+	var records = Any("test", s)
+	var buf = new(bytes.Buffer)
+	for _, record := range records {
+		fmt.Fprintln(buf, record.Key(), record.String())
+	}
+
+	const expected = `test.Foo 12
+test.Bar fgh
+test.Buzz true
+test.testStruct2.A 123
+test.testStruct2.T[0] 8
+test.testStruct2.T[1] 98
+`
+	var actual = buf.String()
+	buf.WriteTo(os.Stdout)
+
+	require.Equal(t, expected, actual)
+}
