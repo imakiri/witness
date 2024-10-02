6417bafae9e914c5f24f9aa052dd023b4ec4bddb
imakiri <vanzha.alexander@gmail.com>
2024-10-03 00:19:34 +0600

    added tee observer

---
+++ observers/tee/tee.go
@@ -0,0 +1,21 @@
+package tee
+
+import (
+	"context"
+	"github.com/gofrs/uuid/v5"
+	"github.com/imakiri/witness"
+)
+
+type Observer struct {
+	observers []witness.Observer
+}
+
+func NewObserver(observers ...witness.Observer) Observer {
+	return Observer{observers: observers}
+}
+
+func (o Observer) Observe(ctx context.Context, spanID uuid.UUID, eventType witness.EventType, eventName string, eventValue string, records ...witness.Record) {
+	for _, observer := range o.observers {
+		observer.Observe(ctx, spanID, eventType, eventName, eventValue, records...)
+	}
+}
