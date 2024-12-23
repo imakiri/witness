94ecb38951f7b2fbe107793f3ca6ef992a3256ad
imakiri <vanzha.alexander@gmail.com>
2024-12-23 16:03:26 +0600

    wip

--- context.go
+++ context.go
@@ -5,17 +5,34 @@ import (
 	"github.com/gofrs/uuid/v5"
 )
 
-type Context struct {
-	contextName string
-	observer    Observer
-	spanIDs     []uuid.UUID
+type ContextSpan struct {
+	spanName string
+	spanID   uuid.UUID
 }
 
-func NewContext(c Context, contextName string, spanIDs ...uuid.UUID) Context {
-	return Context{
-		contextName: contextName,
-		observer:    c.observer,
-		spanIDs:     spanIDs,
+type ContextSpans []ContextSpan
+
+func (cs ContextSpans) FindIDs(spanName ...string) []uuid.UUID {
+	var spanIDs = make([]uuid.UUID, len(spanName))
+	for i := range spanName {
+		for j := range cs {
+			if cs[j].spanName == spanName[i] {
+				spanIDs = append(spanIDs, cs[j].spanID)
+			}
+		}
+	}
+	return spanIDs
+}
+
+type Context struct {
+	observer Observer
+	spans    ContextSpans
+}
+
+func NewContextSpan(spanName string) ContextSpan {
+	return ContextSpan{
+		spanName: spanName,
+		spanID:   uuid.Must(uuid.NewV7()),
 	}
 }
 
@@ -26,25 +43,26 @@ func (c Context) observe(ctx context.Context, skip, extra int, eventType EventTy
 	//eventValue = eventValue[:min(len(eventValue), MaxLengthEventValue)]
 	if debug {
 		//eventCallerPath = trim(eventCallerPath, MaxLengthEventCaller)
-		c.observer.Observe(ctx, c.spanIDs, eventType, eventName, eventCallerPath, records...)
+		c.observer.Observe(ctx, c.SpanIDs(), eventType, eventName, eventCallerPath, records...)
 	} else {
 		//eventCallerName = trim(eventCallerName, MaxLengthEventCaller)
-		c.observer.Observe(ctx, c.spanIDs, eventType, eventName, eventCallerName, records...)
+		c.observer.Observe(ctx, c.SpanIDs(), eventType, eventName, eventCallerName, records...)
 	}
 }
 
 type Finish func(records ...Record)
 
 func (c Context) span(ctx context.Context, contextName, spanName string, records ...Record) (context.Context, Finish) {
-	var childContext = NewContext(c, contextName, uuid.Must(uuid.NewV7()))
-	c.Append(childContext).observe(ctx, 2, 0, EventTypeSpanStart(), spanName, records...)
-	return With(ctx, childContext), func(records ...Record) {
-		childContext.Append(c).observe(ctx, 1, 0, EventTypeSpanFinish(), spanName, records...)
+	var childSpan = NewContextSpan(contextName)
+	c = c.Append(childSpan)
+	c.observe(ctx, 2, 0, EventTypeSpanStart(), spanName, records...)
+	return With(ctx, c), func(records ...Record) {
+		childSpan.Append(c).observe(ctx, 1, 0, EventTypeSpanFinish(), spanName, records...)
 	}
 }
 
 func (c Context) serviceSpan(ctx context.Context, contextName, spanName string, records ...Record) (context.Context, Finish) {
-	var childContext = NewContext(c, contextName, uuid.Must(uuid.NewV7()))
+	var childContext = NewContextSpan(spanName)
 	c.Append(childContext).observe(ctx, 2, 0, EventTypeServiceBegin(), spanName, records...)
 	return With(ctx, childContext), func(records ...Record) {
 		childContext.Append(c).observe(ctx, 1, 0, EventTypeServiceEnd(), spanName, records...)
@@ -52,7 +70,7 @@ func (c Context) serviceSpan(ctx context.Context, contextName, spanName string, 
 }
 
 func (c Context) IsNil() bool {
-	return c.contextName == ""
+	return c.observer == nil || c.spans == nil
 }
 
 func (c Context) Observe(ctx context.Context, eventType EventType, eventName string, records ...Record) {
@@ -63,54 +81,40 @@ func (c Context) Span(ctx context.Context, contextName, spanName string, records
 	return c.span(ctx, contextName, spanName, records...)
 }
 
-// Append appends child context to current context and returns new context
-func (c Context) Append(cc Context) Context {
+// Append appends child context spans to current context and returns new context
+func (c Context) Append(cs ...ContextSpan) Context {
 	return Context{
 		observer: c.observer,
-		spanIDs:  append(c.spanIDs, cc.spanIDs...),
+		spans:    append(c.spans, cs...),
 	}
-}
-
-// Reverse reverses span list, making a parent a child, and a child a parent, and returns new context
-func (c Context) Reverse() Context {
-	var cx = c
-	cx.spanIDs = make([]uuid.UUID, len(c.spanIDs))
-	// slices.Reverse() with copy
-	for i, j := 0, len(cx.spanIDs)-1; i < j; i, j = i+1, j-1 {
-		cx.spanIDs[i], cx.spanIDs[j] = c.spanIDs[j], c.spanIDs[i]
-	}
-	return cx
 }
 
 func (c Context) Observer() Observer {
 	return c.observer
 }
 
-func (c Context) SpanIDs() []uuid.UUID {
-	return c.spanIDs
+func (c Context) Spans() ContextSpans {
+	return c.spans
 }
 
-type Contexts []Context
-
-func (cs Contexts) Context(contextName string) Context {
-	for i := range cs {
-		if cs[i].contextName == contextName {
-			return cs[i]
-		}
+func (c Context) SpanIDs() []uuid.UUID {
+	var spanIDs = make([]uuid.UUID, len(c.spans))
+	for i := range c.spans {
+		spanIDs = append(spanIDs, c.spans[i].spanID)
 	}
-	return Context{observer: NilObserver{}}
+	return spanIDs
 }
 
 const keyContext = "witness.context:3D3DNvuPg4yxitoS0wG8Q0FpI0AeY9BQ"
 
-func With(ctx context.Context, c ...Context) context.Context {
-	return context.WithValue(ctx, keyContext, Contexts(c))
+func With(ctx context.Context, c Context) context.Context {
+	return context.WithValue(ctx, keyContext, c)
 }
 
-func From(ctx context.Context) Contexts {
-	cs, ok := ctx.Value(keyContext).(Contexts)
-	if ok && cs != nil {
+func From(ctx context.Context) Context {
+	cs, ok := ctx.Value(keyContext).(Context)
+	if ok {
 		return cs
 	}
-	return Contexts{{observer: NilObserver{}}}
+	return Context{observer: NilObserver{}}
 }
